8b3292eb5d8ef195eb69a6f5b3cb3ef6af3d102f
Uy DU TRAN <tranuydu@gmail.com>
2018-05-19 21:49:58 +0200

    Adding pop up and button style

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -32,9 +32,9 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary" @click="post">Post
+                            class="postShow" @click="post">Post
                     </button>
-                    <button class="btn btn-primary" @click.prevent="show"> Show Media
+                    <button class="postShow" @click.prevent="show"> Show Media
                     </button>
                 </div>
             </div>
@@ -70,16 +70,21 @@
         },
         methods: {
             post() {
-                const formData = {
-                    name: this.name,
-                    path: this.path,
-                    mediaType: this.mediaType
+                if (this.name != '' && this.path != '' && this.mediaType != '') {
+                    const formData = {
+                        name: this.name,
+                        path: this.path,
+                        mediaType: this.mediaType
+                    }
+                    axios.post('http://localhost:8100/resource-media/', formData).then(res => {
+                        console.log(res)
+                    }).catch(err => {
+                        console.log(err)
+                    })
+                } else {
+                    event.preventDefault()
+                    alert('Please insert all the required fields')
                 }
-                axios.post('http://localhost:8100/resource-media/', formData).then(res => {
-                    console.log(res)
-                }).catch(err => {
-                    console.log(err)
-                })
             },
             show() {
                 this.mediaShow = !this.mediaShow
@@ -174,6 +179,63 @@
         background: -webkit-linear-gradient(top, #D74736, #AD2719);
         background: -moz-linear-gradient(top, #D74736, #AD2719);
         background: -ms-linear-gradient(top, #D74736, #AD2719);
+    }
+
+    label {
+        color: #B4886B;
+        font-weight: bold;
+        display: block;
+        float: left;
+    }
+    label:after { content: ": " }
+
+    h1 { color: #7c795d;
+        font-family: 'Trocchi',
+        serif; font-size: 45px;
+        font-weight: normal;
+        line-height: 48px;
+        margin: 0;
+    }
+
+    .postShow {
+        -moz-box-shadow: 0px 1px 0px 0px #f0f7fa;
+        -webkit-box-shadow: 0px 1px 0px 0px #f0f7fa;
+        box-shadow: 0px 1px 0px 0px #f0f7fa;
+        background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #33bdef), color-stop(1, #019ad2));
+        background:-moz-linear-gradient(top, #33bdef 5%, #019ad2 100%);
+        background:-webkit-linear-gradient(top, #33bdef 5%, #019ad2 100%);
+        background:-o-linear-gradient(top, #33bdef 5%, #019ad2 100%);
+        background:-ms-linear-gradient(top, #33bdef 5%, #019ad2 100%);
+        background:linear-gradient(to bottom, #33bdef 5%, #019ad2 100%);
+        filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#33bdef', endColorstr='#019ad2',GradientType=0);
+        background-color:#33bdef;
+        -moz-border-radius:6px;
+        -webkit-border-radius:6px;
+        border-radius:6px;
+        border:1px solid #057fd0;
+        display:inline-block;
+        cursor:pointer;
+        color:#ffffff;
+        font-family:Arial;
+        font-size:15px;
+        font-weight:bold;
+        padding:6px 24px;
+        text-decoration:none;
+        text-shadow:0px -1px 0px #5b6178;
+    }
+    .postShow:hover {
+        background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #019ad2), color-stop(1, #33bdef));
+        background:-moz-linear-gradient(top, #019ad2 5%, #33bdef 100%);
+        background:-webkit-linear-gradient(top, #019ad2 5%, #33bdef 100%);
+        background:-o-linear-gradient(top, #019ad2 5%, #33bdef 100%);
+        background:-ms-linear-gradient(top, #019ad2 5%, #33bdef 100%);
+        background:linear-gradient(to bottom, #019ad2 5%, #33bdef 100%);
+        filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#019ad2', endColorstr='#33bdef',GradientType=0);
+        background-color:#019ad2;
+    }
+    .postShow:active {
+        position:relative;
+        top:1px;
     }
 
 </style>
