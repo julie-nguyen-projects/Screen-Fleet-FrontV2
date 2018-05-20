cde7a7155d22cf7cde00606b83d39e0034fff793
Julie Nguyen <julie_nguyen@live.fr>
2018-05-20 11:03:12 +0200

    prevent click

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -32,7 +32,7 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="postShow" @click="post">Post
+                            class="postShow" @click.prevent="post">Post
                     </button>
                     <button class="postShow" @click.prevent="show"> Show Media
                     </button>
