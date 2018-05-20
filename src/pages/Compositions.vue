185a44db0c465e591a758552e0774cb70aab5761
Julie Nguyen <julie_nguyen@live.fr>
2018-05-20 22:04:16 +0200

    Add baseModule and medias

--- src/pages/Compositions.vue
+++ src/pages/Compositions.vue
@@ -11,9 +11,11 @@
         <div class="row" style="min-height: 500px">
             <div class="col-md-6">
                 <h4>Aperçu :</h4>
+
                 <p v-if="!compo.id">Votre composition n'est pas encore créée</p>
                 <p v-if="compo.id">{{compo}}</p>
-                <div v-if="compo.module && compo.module.type === '.SplitView'" id="paneSplit" class='components-container' style="min-height: 400px;">
+
+                <div v-if="compo.module && compo.module.type === '.SplitView'" id="paneSplit" v-html="getTemplate()" class='components-container' style="min-height: 400px;">
                     <split-pane v-on:resize="resize" :min-percent='20' v-bind:split="getSplitType(compo.module)">
 
                         <!--
@@ -51,15 +53,19 @@
                 </div>
             </div>
             <div class="col-md-6">
-                <div style="min-height: 350px; max-height: 350px; overflow: auto">
-                    <div class="row">
-                        <div class="col-md-12" style="padding-bottom: 30px">
-                            <p>Diviser l'écran de manière :</p>
+                <div style="min-height: 250px; max-height: 250px; overflow-y: auto">
+                    <div class="row col-md-12">
+                        <div class="col-md-12" style="padding-bottom: 30px"
+                             v-if="(compo.module && compo.module.type === '.SplitView') || !compo.module">
+                            <h4>Diviser l'écran de manière :</h4>
                             <p><button class="btn btn-default col-md-6" @click="addHorizSplit">Horizontale</button></p>
                             <p><button class="btn btn-default col-md-6" @click="addVerticSplit">Verticale</button></p>
                         </div>
-                        <div class="col-md-12">
-                            SLIDE
+                        <div class="col-md-12" v-if="!isThereSlideModule">
+                            <h4>Slide</h4>
+                            <label for="nbSlides">Nombre de slides à ajouter : </label>
+                            <input class="form-control" type="text" id="nbSlides" ref="nbSlides"/>
+                            <button class="btn btn-default" @click="addSlide">Ajouter un module slide</button>
                         </div>
                     </div>
 
@@ -71,12 +77,14 @@
                             <th> Nom </th>
                             <th> Aperçu </th>
                             <th> Type </th>
+                            <th> Ajouter </th>
                         </tr>
                         <tr v-for="media in medias">
                             <td>{{media.name}}</td>
                             <td><a :href="media.path" v-if="media.path != null" target="_blank">Voir le média</a>
                             </td>
                             <td> {{media.mediaType }}</td>
+                            <td><button class="btn btn-default" @click="addMedia(media)">Ajouter</button> </td>
                         </tr>
                     </table>
                 </div>
@@ -99,15 +107,15 @@
         data() {
             return {
                 compo: {id: ''},
-                medias: this.getMedias()
+                medias: this.getMedias(),
+                isThereSlideModule: false
             }
         },
         methods: {
-            getHorizontalSplit() {
-
-            },
-            getVerticalSplit() {
-
+            getTemplate() {
+                console.log('texte');
+                return `<split-pane v-on:resize="resize" :min-percent=\'20\' v-bind:split="` + this.getSplitType(this.compo.module) + `">
+                </split-pane>`
             },
             getSplitType(mod) {
                 return mod.typeSplit.toLowerCase();
@@ -146,6 +154,37 @@
                     })
                     .then((compoUpdated) => this.compo = compoUpdated);
             },
+            addSlide() {
+                console.log(this.$refs.nbSlides.value);
+                if (this.$refs.nbSlides.value === '' || this.$refs.nbSlides.value === '' < 0) {
+                    alert("Nombre de slides incorrect : champ obligatoire qui doit être supérieur à 0");
+                    return false;
+                }
+
+                this.isThereSlideModule = true;
+                let slideModule = {
+                    'type': '.SlideView',
+                    'nbSlides' : this.$refs.nbSlides.value
+                };
+                this.compo['module'] = slideModule;
+                this.addCompo();
+            },
+            addMedia(media) {
+                let baseModule = {
+                    'type': '.BaseModule',
+                    'mediaId': media.id
+                };
+                if (this.compo['module']) {
+                    this.$http.post('http://localhost:8200/base-modules', baseModule)
+                        .then(response => {
+                            const baseModuleCreated = response.body;
+                            this.compo['module'].slides.push(baseModuleCreated);
+                    });
+                } else {
+                    this.compo['module'] = baseModule;
+                    this.addCompo();
+                }
+            },
             addHorizSplit() {
                 let horizSpli = {
                     "type": ".SplitView",
@@ -158,7 +197,6 @@
                     console.log(this.compo['module'].id);
                     if (!this.compo['module'].id || (this.compo['module'].id && this.compo['module'].type !== '.SplitView')) {
                         console.log('%%%% IF');
-
                         this.$http.post('http://localhost:8200/split-views', horizSpli)
                             .then((mod) => {
                                 const module =  mod.body;
